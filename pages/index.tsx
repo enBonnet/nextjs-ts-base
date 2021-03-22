a8a43e1f79a6cd47abe72de78a551454c323501a
Ender Bonnet <enbonnet@gmail.com>
2021-03-21 21:01:44 -0300

    config tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,13 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Titurlo</title>
+        <title>Titulo</title>
       </Head>
       <section>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        <p className="text-purple-600">
+          Hola este es un _boilerplate_ de{" "}
+          <a href="https://nextjs.org/learn">Next.js</a>.
         </p>
-      </section>
-      <section>
-        <h2>Blog</h2>
       </section>
     </div>
   );
