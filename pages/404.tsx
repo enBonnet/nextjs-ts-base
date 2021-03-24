3c33adc3b4878ab3638988f70f786138553e24ee
Ender Bonnet <enbonnet@gmail.com>
2021-03-23 22:43:05 -0300

    config error and 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,26 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function FourOhFour() {
   return (
-    <>
-      <h1>404 - Page Not Found</h1>
-      <Link href="/">
-        <a>Go back home</a>
-      </Link>
-    </>
+    <div className="flex items-center justify-center h-screen background">
+      <Head>
+        <title>404 - Page Not Found</title>
+      </Head>
+      <section className="p-8 rounded bg-yellow-50 mx-4">
+        <h1 className="text-center mb-8">
+          <span className="block text-5xl font-bold leading-none">
+            404 - Page Not Found
+          </span>
+        </h1>
+        <div className="text-center">
+          <Link href="/">
+            <a className="focus:outline-none text-white text-sm py-2.5 px-5 border-b-4 border-blue-600 rounded-md bg-blue-500 hover:bg-blue-400">
+              Go back home
+            </a>
+          </Link>
+        </div>
+      </section>
+    </div>
   );
 }
--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,12 +1,26 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Error() {
   return (
-    <>
-      <h1>Error Page</h1>
-      <Link href="/">
-        <a>Go back home</a>
-      </Link>
-    </>
+    <div className="flex items-center justify-center h-screen background">
+      <Head>
+        <title>Error Page</title>
+      </Head>
+      <section className="p-8 rounded bg-yellow-50 mx-4">
+        <h1 className="text-center mb-8">
+          <span className="block text-5xl font-bold leading-none">
+            Error Page
+          </span>
+        </h1>
+        <div className="text-center">
+          <Link href="/">
+            <a className="focus:outline-none text-white text-sm py-2.5 px-5 border-b-4 border-blue-600 rounded-md bg-blue-500 hover:bg-blue-400">
+              Go back home
+            </a>
+          </Link>
+        </div>
+      </section>
+    </div>
   );
 }
